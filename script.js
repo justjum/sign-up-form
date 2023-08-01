36a9fc7f6fcc6151ca2d6393eff8fe3675834758
justjum <jumasaurusrex@gmail.com>
2023-08-02 07:28:19 +0800

    Javascript updated for shorter form - Less repeatability - Error messages linked to password boxes

--- script.js
+++ script.js
@@ -2,7 +2,61 @@
 
 let passwordEntry = document.getElementById("password");
 let passwordConfirm = document.getElementById("confirm-password").value;
-let passwordMatch = "";
+let error1 = "";
+let error2 = "";
+
+
+const enterPassword = document.querySelector(".password");
+
+
+enterPassword.addEventListener('keyup', function() {
+    passwordEntry = document.getElementById("password").value;
+    passwordConfirm = document.getElementById("confirm-password").value;
+    passwordLength(passwordEntry);
+    passwordMatch(passwordEntry, passwordConfirm);
+    document.getElementById("password-length").innerHTML=`${error1}`;
+    document.getElementById("password-match").innerHTML=`${error2}`;
+});
+
+const confirmPassword = document.querySelector(".confirm-password");
+
+confirmPassword.addEventListener('keyup', function() {
+    passwordEntry = document.getElementById("password").value;
+    passwordConfirm = document.getElementById("confirm-password").value;
+    passwordLength(passwordEntry);
+    passwordMatch(passwordEntry, passwordConfirm);
+    document.getElementById("password-length").innerHTML=`${error1}`;
+    document.getElementById("password-match").innerHTML=`${error2}`;
+});
+
+
+const passwordLength = function(currentPassword) {
+    if(currentPassword.length < 8) {
+        error1 = "Password too short"
+    }
+    else {
+        error1 = "";
+    }
+    return error1;
+}
+
+const passwordMatch = function(passwordEntry, passwordConfirm) {
+    if(passwordConfirm === "") {
+        error2 = "";
+    }
+    else if (passwordConfirm === passwordEntry) {
+        error2 = "";
+    }
+    else {
+        error2 = "Passwords do not match"
+    }
+    return passwordMatch;
+}
+
+
+/*  Below was the original password check.
+- V2 developed to check for both length and match
+- Possibly to also throw permanent error state
 
 const enterPassword = document.querySelector(".password");
 
@@ -36,5 +90,4 @@ confirmPassword.addEventListener('keyup', function() {
         passwordMatch = "Passwords do not match.";
     }
     document.getElementById("password-match").innerHTML=`${passwordMatch}`;
-});
-
+}); */
